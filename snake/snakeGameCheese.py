4ec8979628affc8b4408fec1a98512c9681a2ecc
CLiang531 <liangc30@uci.edu>
2026-03-04 00:35:05 -0800

    bug with length comparison when grow_tail is negative

--- snake/snakeGameCheese.py
+++ snake/snakeGameCheese.py
@@ -191,7 +191,7 @@ class snakeGameCheese():
                 self.fruit_eaten(i, fruit)
 
         if self.grow_tail < 0:
-            if abs(self.grow_tail) >= len(self.snake_body):
+            if len(self.snake_body) + self.grow_tail <= 1:
                 if self.DEBUG:
                     with open(self.log, "a") as f:
                         print("FRUIT DEATH, DEDUCTION LONGER THAN CURRENT BODY", file=f)
